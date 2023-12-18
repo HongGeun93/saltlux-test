b14f28d20f04cdd8635bb53303760563d05d9331
hongs <ghdrmsg@gmail.com>
2023-12-18 17:52:11 +0900

    hello

--- src/main/java/com/example/hongs/HongsApplication.java
+++ src/main/java/com/example/hongs/HongsApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HongsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HongsApplication.class, args);
+		System.out.println("hong");
 	}
 
 }
