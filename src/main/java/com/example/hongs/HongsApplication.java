67f8caf5208285310521a7bf85d6a0a95d0d9c44
곽홍근 <honggeun.kwak@saltlux.com>
2023-12-18 17:45:39 +0900

    프로젝트 초기화

---
+++ src/main/java/com/example/hongs/HongsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hongs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hongs/HongsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hongs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
