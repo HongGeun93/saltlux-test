d1aad176c9b1b745e273ec9a091c569a34c51814
hongs <ghdrmsg@gmail.com>
2023-12-22 16:41:07 +0900

    테스트 controller 추가

--- src/main/java/com/example/hongs/HongsApplication.java
+++ src/main/java/com/example/hongs/HongsApplication.java
@@ -1,14 +1,18 @@
 package com.example.hongs;
 
+import com.example.hongs.domain.Hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HongsApplication {
 
-	public static void main(String[] args) {
-
-		SpringApplication.run(HongsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Hello hellosss = new Hello();
+        hellosss.setData("hello");
+        String data = hellosss.getData();
+        SpringApplication.run(HongsApplication.class, args);
+		System.out.println("data = " + data);
+    }
 
 }
