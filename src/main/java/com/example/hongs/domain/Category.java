c5a217147d6736e5fba4f7dc1298ccb38c5949ef
hongs <ghdrmsg@gmail.com>
2023-12-26 15:42:57 +0900

    xToOne 은 무조건 지연로딩(LAZY)으로 설정

--- src/main/java/com/example/hongs/domain/Category.java
+++ src/main/java/com/example/hongs/domain/Category.java
@@ -3,6 +3,7 @@ package com.example.hongs.domain;
 import com.example.hongs.domain.item.Item;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -34,7 +35,7 @@ public class Category {
         inverseJoinColumns = @JoinColumn(name = "item_id"))
     private List<Item> items = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
