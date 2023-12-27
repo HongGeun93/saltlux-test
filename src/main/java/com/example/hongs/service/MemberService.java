3da95966a7bc700e736571c9cc938a259d862b2a
hongs <ghdrmsg@gmail.com>
2023-12-27 10:09:35 +0900

    member service 구현

---
+++ src/main/java/com/example/hongs/service/MemberService.java
@@ -0,0 +1,46 @@
+package com.example.hongs.service;
+
+import com.example.hongs.domain.Member;
+import com.example.hongs.repository.MemberRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원가입
+     */
+    @Transactional
+    public Long join(Member member) throws IllegalAccessException {
+        // 중복회원
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 회원전체 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+    private void validateDuplicateMember(Member member) throws IllegalAccessException {
+        // exception
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()) {
+            throw new IllegalAccessException("이미 존재하는 회원입니다.");
+        }
+    }
+}
